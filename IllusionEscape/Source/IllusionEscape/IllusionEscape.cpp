9bdc01e4bb12ec7d896077881a210ee59a4b2357
jichumao <jichumao@qq.com>
2025-03-02 00:32:25 -0500

    init

---
+++ IllusionEscape/Source/IllusionEscape/IllusionEscape.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "IllusionEscape.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IllusionEscape, "IllusionEscape" );
+ 
---
+++ IllusionEscape/Source/IllusionEscape/IllusionEscapeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IllusionEscapeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AIllusionEscapeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AIllusionEscapeGameMode();
+};
+
+
+
